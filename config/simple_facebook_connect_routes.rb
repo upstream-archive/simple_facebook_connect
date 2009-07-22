e596ef202a1d08b3c006c03678322f78e306448f
Alexander Lang <alex@upstream-berlin.com>
2009-07-22 15:12:42 +0200

    ruby 1.8 compatibility

--- config/simple_facebook_connect_routes.rb
+++ config/simple_facebook_connect_routes.rb
@@ -1,4 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.fb_connect '/fb/connect', :controller => 'simple_facebook_connect/connect', action: 'connect'
-  map.fb_authenticate '/fb/authenticate', :controller => 'simple_facebook_connect/connect', action: 'authenticate'
+  map.fb_connect '/fb/connect', :controller => 'simple_facebook_connect/connect', :action => 'connect'
+  map.fb_authenticate '/fb/authenticate', :controller => 'simple_facebook_connect/connect', :action => 'authenticate'
 end
